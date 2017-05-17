d498550697dc339807bc5573d36a4b80b4783e2b
vagrant <vagrant@homestead>
2017-05-17 09:09:49 +0000

    uploaded files

---
+++ about.php
@@ -0,0 +1,419 @@
+<html lang="en">
+
+	<head>
+	
+		<meta charset="utf-8">
+		<title>All Cards - Startup UI Kit</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link rel="shortcut icon" href="images/favicon.ico">
+		<!-- <link rel="stylesheet" href="bootstrap/css/bootstrap.css"> -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,400i,700,700i|Montserrat:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+		<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/29/0/intl/en_au/common.js"></script>
+		<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/29/0/intl/en_au/map.js"></script>
+		<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/29/0/intl/en_au/util.js"></script>
+		<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/29/0/intl/en_au/marker.js"></script>
+
+		<style type="text/css">.gm-style {font: 400 11px Roboto, Arial, sans-serif; text-decoration: none; } .gm-style img { max-width: none; } 
+		</style>
+		<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/29/0/intl/en_au/onion.js"></script>
+		<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/29/0/intl/en_au/stats.js"></script>
+		<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/29/0/intl/en_au/controls.js"></script>
+	</head>
+	<body>
+
+
+		<section class="cover-1 text-center">
+			<nav class="navbar navbar-toggleable-sm navbar-custom navbar-inverse">
+				<div class="container">
+					<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<a class="navbar-brand" href="#">Startup UI Kit</a>
+					<div class="collapse navbar-collapse pull-xs-right justify-content-end" id="navbarToggler">
+						<ul class="navbar-nav mt-2 mt-md-0">
+							<li class="nav-item active">
+								<a class="nav-link" href="#welcome">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#about">About</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#work">Our Work</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#news">Blog</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#contact">Contact</a>
+							</li>
+							<li class="nav-item">
+								<a class="btn btn-outline-white btn-outline" href="#newsletter">Sign Up</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div class="cover-container pb-5" id="welcome">
+				<div class="cover-inner container">
+						<h1 class="jumbotron-heading">Say <em>hello</em> to our  <strong>Startup UI Kit</strong></h1>
+						<p class="lead">A collection of coded HTML and CSS elements to help your build your startup website. Clean design, fully responsive and based on Bootstrap 4.</p>
+						<p>
+							<a href="#" class="btn btn-primary btn-lg mb-2 mr-2 ml-2">Main call to action</a>
+							<a href="#" class="btn btn-outline-white btn-lg mb-2 ml-2 ml-2">Secondary action</a>
+						</p>
+				</div>
+			</div>
+		</section>
+
+		<!-- Features -->
+
+		<section class="features-4 text-center" id="services">
+			<div class="container">
+				<div class="row justify-center">
+					<div class="col-md-8 text-center">
+						<h2>Features</h2>
+						<p class="lead mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus erat sed sem sagittis cursus.</p>
+						<div class="divider"></div>
+					</div>	
+				</div>
+				<div class="row">
+					<div class="col-md-3 col-feature">
+						<div class="rounded-circle justify-center">
+							<em class="fa fa-2x fa-paint-brush"></em>
+						</div>
+						<h4>Clean Design</h4>
+						<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+					</div>
+					<div class="col-md-3 col-feature">
+						<div class="rounded-circle justify-center">
+							<em class="fa fa-2x fa-mobile"></em>
+						</div>
+						<h4>Responsive</h4>
+						<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+					</div>
+					<div class="col-md-3 col-feature">
+						<div class="rounded-circle justify-center">
+							<em class="fa fa-2x fa-code"></em>
+						</div>
+						<h4>Bootstrap 4</h4>
+						<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+					</div>
+					<div class="col-md-3 col-feature">
+						<div class="rounded-circle justify-center">
+							<em class="fa fa-2x fa-pencil"></em>
+						</div>
+						<h4>Easy to Edit</h4>
+						<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Content block -->
+
+		<section class="content-5" id="about">
+			<div class="container">
+				<h2 class="text-center">Content Block</h2>
+				<div class="divider"></div>
+				<div class="row justify-center mt-5">
+					<div class="col-md-6 pl-5 pr-5 text-center">
+						<img class="mb-4 img-fluid" src="images/placeholder-desk.jpg">
+					</div>
+					<div class="col-md-6 text-left">
+						<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+						<p class="mt-4 mb-4">Integer libero nunc, elementum eu venenatis sed, molestie non tortor. Duis suscipit pulvinar justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec semper purus ex. Curabitur non rutrum libero. Fusce lacus tortor, elementum ac gravida vitae, egestasr.</p>
+						
+						<p><a class="btn btn-primary mr-2" href="#" role="button">Get Started</a>
+						<a class="btn btn-outline-secondary" href="#" role="button">Find out more</a></p>
+						
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Call to action -->
+
+		<section class="cta-1 text-center" id="newsletter">
+			<div class="container">
+				<h2>Join us today</h2>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<form class="subscribe">
+					<div class="form-group row pt-5 pb-2">
+						<div class="col-md-4 mb-3">
+							<input type="text" class="form-control-custom" id="inputName" placeholder="Your Name">
+						</div>
+						<div class="col-md-4 mb-3">
+							<input type="email" class="form-control-custom" id="inputEmail" placeholder="Your Email">
+						</div>
+						<div class="col-md-4">
+							<button type="submit" class="btn btn-lg btn-primary">Notify Me</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</section>
+
+		<!-- another call to action -->
+
+		<section class="cta-3 text-center justify-center">
+			<div class="container">
+				<h2 class="mb-4">Download this UI Kit and use it to help your startup get started up</h2>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<button type="submit" class="btn btn-lg btn-primary mt-5">Buy Now</button>
+			</div>
+		</section>
+
+		<!-- Meet the team -->
+
+		<section class="team-3 text-center alt-background" id="team">
+			<div class="container">
+				<h2>Meet our Team</h2>
+				<p class="lead mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<div class="row">
+					<div class="col-md-4 col-team">
+						<div class="box rounded-corners">
+							<img class="rounded-circle" src="images/team-1.jpg">
+							<h4>Jonathan Smith</h4>
+							<p><span class="emphasis">Designer</span></p>
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<ul class="social">
+								<li><a href="#" title="Facebook" class="fa fa-facebook"></a></li>
+								<li><a href="#" title="Twitter" class="fa fa-twitter"></a></li>
+								<li><a href="#" title="Google+" class="fa fa-google"></a></li>
+								<div class="clear"></div>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-4 col-team">
+						<div class="box rounded-corners">	
+							<img class="rounded-circle" src="images/team-2.jpg">
+							<h4>Jane Donovan</h4>
+							<p><span class="emphasis">Designer</span></p>
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<ul class="social">
+								<li><a href="#" title="Facebook" class="fa fa-facebook"></a></li>
+								<li><a href="#" title="Twitter" class="fa fa-twitter"></a></li>
+								<li><a href="#" title="Google+" class="fa fa-google"></a></li>
+								<div class="clear"></div>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-4 col-team">
+						<div class="box rounded-corners">	
+							<img class="rounded-circle" src="images/team-3.jpg">
+							<h4>Sam Davidson</h4>
+							<p><span class="emphasis">Designer</span></p>
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<ul class="social">
+								<li><a href="#" title="Facebook" class="fa fa-facebook"></a></li>
+								<li><a href="#" title="Twitter" class="fa fa-twitter"></a></li>
+								<li><a href="#" title="Google+" class="fa fa-google"></a></li>
+								<div class="clear"></div>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+
+
+
+		<!-- gallery or our work -->
+
+		<section class="gallery-2 text-center" id="work">
+			<div class="container-fluid">
+				<h2>See our Work</h2>
+				<p class="lead constrain-width">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus erat sed sem sagittis cursus. Sed mollis neque at aliquam rhoncus. Donec magna turpis, dictum sit amet tellus at, commodo elementum sapien.</p>
+				<div class="divider"></div>
+				<div class="row mt-5 no-gutters">
+					<div class="col-lg-3 item" style="background-image: url(images/placeholder-image-2.jpg); height: 400px;">
+					</div>
+					<div class="col-lg-6 item" style="background-image: url(images/placeholder-image-6.jpg); height: 400px;">
+					</div>
+					<div class="col-lg-3 item" style="background-image: url(images/placeholder-image-4.jpg); height: 400px;">
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+
+
+		<!-- latest news -->
+
+		<section class="gallery-4 text-center" id="news">
+			<div class="container">
+				<h3>Latest News</h3>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<div class="divider"></div>
+				<div class="row text-left">
+					<div class="col-sm-6 col-md-4 col-gallery">
+						<div class="box">
+							<img src="images/placeholder-image-1.jpg" class="img-fluid rounded-corners">
+							<h5 class="mt-4 ml-4 mr-4">Example Blog Post</h5>
+							<p class="emphasis ml-4 mr-4">17th of May, 2017</p>
+							<p class="ml-4 mr-4">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<p class="emphasis ml-4 mr-4"><a href="#">Read more</a></p>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4 col-gallery">
+						<div class="box">
+							<img src="images/placeholder-image-2.jpg" class="img-fluid rounded-corners">
+							<h5 class="mt-4 ml-4 mr-4">Example Blog Post</h5>
+							<p class="emphasis ml-4 mr-4">17th of May, 2017</p>
+							<p class="ml-4 mr-4">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<p class="emphasis ml-4 mr-4"><a href="#">Read more</a></p>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4 col-gallery">
+						<div class="box">
+							<img src="images/placeholder-image-3.jpg" class="img-fluid rounded-corners">
+							<h5 class="mt-4 ml-4 mr-4">Example Blog Post</h5>
+							<p class="emphasis ml-4 mr-4">17th of May, 2017</p>
+							<p class="ml-4 mr-4">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<p class="emphasis ml-4 mr-4"><a href="#">Read more</a></p>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4 col-gallery">
+						<div class="box">
+							<img src="images/placeholder-image-4.jpg" class="img-fluid rounded-corners">
+							<h5 class="mt-4 ml-4 mr-4">Example Blog Post</h5>
+							<p class="emphasis ml-4 mr-4">17th of May, 2017</p>
+							<p class="ml-4 mr-4">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<p class="emphasis ml-4 mr-4"><a href="#">Read more</a></p>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4 col-gallery">
+						<div class="box">
+							<img src="images/placeholder-image-5.jpg" class="img-fluid rounded-corners">
+							<h5 class="mt-4 ml-4 mr-4">Example Blog Post</h5>
+							<p class="emphasis ml-4 mr-4">17th of May, 2017</p>
+							<p class="ml-4 mr-4">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<p class="emphasis ml-4 mr-4"><a href="#">Read more</a></p>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4 col-gallery">
+						<div class="box">
+							<img src="images/placeholder-image-6.jpg" class="img-fluid rounded-corners">
+							<h5 class="mt-4 ml-4 mr-4">Example Blog Post</h5>
+							<p class="emphasis ml-4 mr-4">17th of May, 2017</p>
+							<p class="ml-4 mr-4">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+							<p class="emphasis ml-4 mr-4"><a href="#">Read more</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Reviews  -->
+
+		<section class="testimonial-4 text-center" id="reviews">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 offset-sm-2">
+						<blockquote>
+							<p class="quote-text">"We shall not cease from exploration, and the end of all our exploring will be to arrive where we started and know the place for the first time."</p>
+							<p><span class="quote-author">T. S. Elliot</span></p>
+						</blockquote>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<!-- Contact us -->
+
+
+		<section class="contact-1" id="contact">
+			<div class="container">
+				<div class="row contact-details">
+					<div class="col-sm-8 text-center text-md-left mb-4">
+						<h3>Ask us a question</h3>
+						<form class="contact-form mt-4">
+							<div class="row">
+								<div class="col-md-5">
+									<input type="text" class="form-control-custom mb-4" placeholder="Name" value="Your name">
+								</div>
+								<div class="col-md-5">
+									<input type="text" class="form-control-custom mb-4" placeholder="Email address" value="Email address">
+								</div>
+								<br>
+							</div>
+							<div class="row">
+								<div class="col-md-10">
+									<textarea class="form-control-custom mb-4" rows="3">Your Message</textarea><br>
+									<button type="submit" class="btn btn-primary btn-lg mb-4">Send Message</button>
+								</div> 
+							</div>
+						</form>
+					</div>
+					<div class="col-sm-4 text-center text-md-left">
+						<h3>Contact</h3>
+						<h4 class="pt-4">Email</h4>
+						<p>hello@startup.co</p>
+						<h4 class="pt-2">Phone</h4>
+						<p>+111 234 567 89</p>
+						<h4 class="pt-2">Address</h4>
+						<p>1 Street Name, City, Zip Code<br>
+						United States</p>
+						<ul class="social">
+							<li><a href="#" title="Facebook" class="fa fa-facebook"></a></li>
+							<li><a href="#" title="Twitter" class="fa fa-twitter"></a></li>
+							<li><a href="#" title="Google+" class="fa fa-google"></a></li>
+							<li><a href="#" title="Dribbble" class="fa fa-dribbble"></a></li>
+							<li><a href="#" title="Instagram" class="fa fa-instagram"></a></li>
+							<div class="clear"></div>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d17752.017372427184!2d176.25029293829252!3d-38.13686853320569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snz!4v1495010276219" width="2000" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+
+		<!-- footer -->
+
+
+		<footer class="footer-2" id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 text-center text-md-left">
+						<a class="logo" href="#">Startup UI Kit</a>
+					</div>
+					<div class="col-md-8">
+						<ul class="nav-footer mt-2 mt-md-0 ">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">About</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Our Work</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+	
+	</body>
+</html>
